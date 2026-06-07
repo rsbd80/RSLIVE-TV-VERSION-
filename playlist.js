c32d0e69d70db156e1c8738e44d5aa9d3bf24ccc
Tv-bd2 <rspremium64@gmail.com>
2026-06-07 20:17:50 +0600

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -2,9 +2,9 @@ const channels = [
 
 {
 name:"T Sports",
-logo:"https://i.imgur.com/3ZQ3Z5G.png",
+logo:"https://i.postimg.cc/JhKqC77G/20260607-050007.jpg",
 category:"Sports",
-url:"https://rs-player.netlify.app/jw-player?url=https://amitomar.bdixbd.net/hoichoi.pro1/tracks-v1a1/index.m3u8"
+url:"https://amitomar.bdixbd.net/hoichoi.pro1/tracks-v1a1/index.m3u8"
 },
 
 {
